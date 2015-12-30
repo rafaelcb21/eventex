336059ff1f000c722f14f53573018d920872b760
rafaelcb21 <rafaelcb21@gmail.com>
2015-12-30 17:32:08 -0200

    Atualiza url para 1.9

--- eventex/urls.py
+++ eventex/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from eventex.core.views import home
+
 
 urlpatterns = {
     url(r'^admin/', admin.site.urls),
-    url(r'^$', 'eventex.core.views.home'),
+    url(r'^$', home),
 }
