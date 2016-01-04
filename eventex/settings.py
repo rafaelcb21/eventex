1fd5bd10e41c488f6f6ebb5039b6845bd3a0dd9d
rafaelcb21 <rafaelcb21@gmail.com>
2016-01-04 10:25:08 -0200

    Refatora geral

--- eventex/settings.py
+++ eventex/settings.py
@@ -24,8 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
+
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
+DEFAULT_FROM_EMAIL = 'rafaelcb21@gmail.com'
 
 # Application definition
 
---
+++ eventex/subscriptions/tests/test_mail_subscribe.py
@@ -0,0 +1,35 @@
+from django.core import mail
+from django.test import TestCase
+
+
+class SubscribePostValid(TestCase):
+    def setUp(self):
+        data = dict(name='Rafael', cpf='12345678901',
+                    email='rafael@braga.net', phone='11-99969-8891')
+        self.client.post('/inscricao/', data)
+        self.email = mail.outbox[0]
+
+    def test_subscription_email_subject(self):
+        expect = 'Confirmação de inscrição'
+
+        self.assertEqual(expect, self.email.subject)
+
+    def test_subscription_email_from(self):
+        expect = 'rafaelcb21@gmail.com'
+
+        self.assertEqual(expect, self.email.from_email)
+
+    def test_subscription_email_to(self):
+        expect = ['rafaelcb21@gmail.com', 'rafael@braga.net']
+
+        self.assertEqual(expect, self.email.to)
+
+    def test_subscription_email_body(self):
+        contents = ['Rafael',
+                    '12345678901',
+                    'rafael@braga.net',
+                    '11-99969-8891'
+                    ]
+        for content in contents:
+            with self.subTest():
+                self.assertIn(content, self.email.body)
