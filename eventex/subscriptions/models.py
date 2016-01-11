498e82531da2f6b98599aa45ce46e918bf5a7062
rafaelcb21 <rafaelcb21@gmail.com>
2016-01-11 01:02:15 -0200

    Adiciona campo paid

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -21,6 +21,7 @@ class Subscription(models.Model):
     phone = models.CharField('telefone', max_length=20)
     created_at = models.DateTimeField('criado em', auto_now_add=True)
     key_hash = models.CharField('hash', max_length=40, default=hash_)
+    paid = models.BooleanField('pago', default=False)
 
     class Meta:
         verbose_name_plural = 'inscrições'
