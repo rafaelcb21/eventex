bd0a26cf23c85005f66db16828e2f4c40a4b590f
rafaelcb21 <rafaelcb21@gmail.com>
2016-01-23 10:11:31 -0200

    Simpifica com modelform

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,43 +1,12 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from eventex.subscriptions.models import Subscription
 
 
-def cpf_bool(cpf, n=0, count=10, digit_str=''):
-    """cpf_bool function validates cpf returning True or False"""
-
-    list_1 = []
-    for num in cpf[:9 + n]:
-        var = int(num) * count
-        list_1.append(var)
-        count -= 1
-
-    digit = sum(list_1) % 11
-    digit = 0 if digit < 2 else 11 - digit
-
-    digits = digit_str + str(digit)
-
-    if len(digits) < 2:
-        return cpf_bool(cpf, 1, 11, digits)
-
-    return True if cpf[:9] + digits == cpf else False
-
-
-def validate_cpf(value):
-    if not value.isdigit():
-        raise ValidationError('CPF deve conter apenas números.', 'digits')
-
-    if len(value) != 11:
-        raise ValidationError('CPF deve ter 11 números.', 'length')
-
-    if cpf_bool(value) is False:
-        raise ValidationError('CPF inválido', 'valid')
-
-
-class SubscriptionForm(forms.Form):
-    name = forms.CharField(label='Nome')
-    cpf = forms.CharField(label='CPF', validators=[validate_cpf])
-    email = forms.EmailField(label='Email', required=False)
-    phone = forms.CharField(label='Telefone', required=False)
+class SubscriptionForm(forms.ModelForm):
+    class Meta:
+        model = Subscription
+        fields = ['name', 'cpf', 'email', 'phone']
 
     def clean_name(self):
         name = self.cleaned_data['name']
@@ -45,6 +14,8 @@ class SubscriptionForm(forms.Form):
         return ' '.join(words)
 
     def clean(self):
+        self.cleaned_data = super().clean()
+
         if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
             raise ValidationError('Informe seu e-mail ou telefone')
 
--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from eventex.subscriptions.validators import validate_cpf
 from hashlib import sha1
 from random import SystemRandom
 from uuid import uuid4
@@ -15,9 +16,9 @@ def hash_(size=15, chars=char):
 
 class Subscription(models.Model):
     name = models.CharField('nome', max_length=100)
-    cpf = models.CharField('CPF', max_length=11)
-    email = models.EmailField('e-mail')
-    phone = models.CharField('telefone', max_length=20)
+    cpf = models.CharField('CPF', max_length=11, validators=[validate_cpf])
+    email = models.EmailField('e-mail', blank=True)
+    phone = models.CharField('telefone', max_length=20, blank=True)
     created_at = models.DateTimeField('criado em', auto_now_add=True)
     key_hash = models.CharField('hash', max_length=40, default=hash_)
     paid = models.BooleanField('pago', default=False)
